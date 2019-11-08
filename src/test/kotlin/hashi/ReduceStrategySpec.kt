d580ba57cacbca74c04201479fc391c8a9983277
wayne.yu <wayne.yu@paytm.com>
2019-11-07 22:27:58 -0500

    Refactor tests

--- src/test/kotlin/hashi/ReduceStrategySpec.kt
+++ src/test/kotlin/hashi/ReduceStrategySpec.kt
@@ -10,71 +10,88 @@ import kotlin.test.assertEquals
 @RunWith(JUnitPlatform::class)
 class ReduceStrategySpec : Spek({
 
+    fun verifyStrategy(initialBoard: String, finalBoard: String, reduceStrategy: ReduceStrategy) {
+        val board =  Board.fromString(initialBoard)
+        val expected =  Board.fromString(finalBoard)
+        val actual = board.let { it -> reduceStrategy.reduceBoard(it) }
+        println("expected\n${expected.printBoard()}\n")
+        println("actual\n${actual.printBoard()}")
+        assertEquals(expected, actual)
+    }
+
     describe("reduceStrategy") {
 
         it("should reduce island with 3 bridges and only 2 neighbors") {
-            val node1 = Node(1, 1, 0, 0)
-            val node2 = Node(2, 2, 4, 4)
-            val node3 = Node(3, 3, 0, 4)
-            val board = Board(5,5, listOf(node1, node2, node3))
-
-            val expected = board.connect(node3, node1).connect(node3, node2)
-            val actual = board.let { it -> MoreThanThreeBridgesAndTwoNeighbors.reduceBoard(it) }
-
-            assertEquals(expected, actual)
+            verifyStrategy(
+            """
+                10003
+                00000
+                00000
+                00000
+                00002
+            """.trimIndent(),
+            """
+                1---3
+                0000-
+                0000-
+                0000-
+                00002
+            """.trimIndent(), MoreThanThreeBridgesAndTwoNeighbors)
         }
 
         it("should reduce island with only one neighbor") {
-            val node1 = Node(1, 1, 0, 0)
-            val node2 = Node(2, 2, 0, 4)
-            val board = Board(5,5, listOf(node1, node2))
-
-            val expected = board.connect(node1, node2)
-            val actual = board.let { it -> OneNonConnectedNeighbor.reduceBoard(it) }
-            assertEquals(expected, actual)
+            verifyStrategy(
+            """
+                10002
+                00000
+                00000
+                00000
+                00000
+            """.trimIndent(),
+            """
+                1---2
+                00000
+                00000
+                00000
+                00000
+            """.trimIndent(), OneNonConnectedNeighbor)
         }
 
         it("should reduce island when all neighbors have same remaining bridges") {
-            val board =  Board.fromString("""
+            verifyStrategy(
+                    """
                 00200
                 00000
                 10400
                 00000
                 002-1
-            """.trimIndent())
-
-            val expected =  Board.fromString("""
+            """.trimIndent(),
+                    """
                 00200
                 00=00
                 1-400
                 00-00
                 002-1
-            """.trimIndent())
-
-            val actual = board.let { NeighborsWithSameRemainingBridges.reduceBoard(it) }
-            println(actual.printBridges())
-            assertEquals(expected.printBoard(), actual.printBoard())
+            """.trimIndent(), NeighborsWithSameRemainingBridges)
         }
 
         it("should reduce island with six bridges and three neighbors"){
-            val board =  Board.fromString("""
+            verifyStrategy(
+                    """
                 00200
                 00000
                 30600
                 00000
                 1-4-1
-            """.trimIndent())
-
-            val expected =  Board.fromString("""
+            """.trimIndent(),
+                    """
                 00200
                 00=00
                 3=600
                 00=00
                 1-4-1
-            """.trimIndent())
-
-            val actual = board.let { SixBridgesIsland.reduceBoard(it) }
-            assertEquals(expected, actual)
+            """.trimIndent(), SixBridgesIsland)
         }
     }
+
 })
